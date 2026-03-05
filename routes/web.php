0429d90754f00c4995bcde90678b8d79a67f7cdf
GitJezequielB <jebueso@protonmail.com>
2026-03-05 17:10:03 -0600

    tiendyn Index

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'name'    => 'Tiendyn Pedidos API',
-        'version' => '1.0.0',
-        'status'  => 'online',
-        'docs'    => url('/api'),
-    ]);
+    return view('welcome');
 });
